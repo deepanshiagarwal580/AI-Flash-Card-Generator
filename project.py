6629a27123f05d174be4258d3e371bee013fc4d2
deepanshiagarwal580 <deepanshiagarwal580@gmail.com>
2026-09-03 14:39:50 +0530

    Update project.py

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # Use your own Google Gemini API key as an environment variable
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_API_KEY = st.secrets("GOOGLE_API_KEY")
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
